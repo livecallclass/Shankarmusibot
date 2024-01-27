1b1797ce607ff709071d3758f82e911df6da6f51
livecallclass <154664537+livecallclass@users.noreply.github.com>
2024-01-28 03:10:50 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", 0))
-API_HASH = getenv("API_HASH", None)
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_ID = int(getenv("OWNER_ID", 57))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "57").split()))
+API_ID = int(getenv("API_ID", 29687224))
+API_HASH = getenv("API_HASH", c543dbfe3007000fbecbd48c10731a55)
+BOT_TOKEN = getenv("BOT_TOKEN", 6723628824:AAHdyEH9Pbmz8N9HJA81umH_e1-8o3MmUOo)
+OWNER_ID = int(getenv("OWNER_ID", 6427042108))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6427042108").split()))
